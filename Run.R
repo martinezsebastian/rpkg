6d0b80e52b3e103d47012857bc6e0e11be9ba5e6
martinezsebastian <sebastian.martinez32@gmail.com>
2019-05-27 14:27:50 -0400

    Added a function and its documentation

--- Run.R
+++ Run.R
@@ -23,3 +23,25 @@ usethis::use_travis()
 
 # sets up testing infrastructure
 usethis::use_testthat()
+
+
+usethis::use_data_raw()
+
+
+
+# load required packages ----
+if (!require("pacman")) install.packages("pacman") 
+pacman::p_load(magrittr, dplyr, usethis, data.table, here)
+
+# clean data ----
+epil <- read.csv(here::here("data-raw","epil.csv"))
+DT <- epil %>% as.data.table
+DT.base <- DT %>% distinct(subject, .keep_all = TRUE)
+DT.base[,`:=`(period=0,y=base)]
+DT.epil <- rbind(DT, DT.base)
+setkey(DT.epil, subject, period)
+DT.epil[,`:=`(post=as.numeric(period>0), tj=ifelse(period==0,8,2))]
+df_epil <- as.data.frame(DT.epil) %>% dplyr::select(y, trt, post, subject, tj)
+
+# write data in correct format to data folder ----
+usethis::use_data(df_epil, overwrite = TRUE)
